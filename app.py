41c0b904dc884fb2557a818f85fe88728f655069
bahao <18252110983@163.com>
2024-12-30 23:23:08 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from pathlib import Path
 from PIL import Image
 from loguru import logger
@@ -8,35 +9,31 @@ import numpy as np
 import streamlit as st
 import cv2
 from streamlit.runtime.uploaded_file_manager import UploadedFile
-from typing import List,Dict
+from typing import List, Dict
 
 import config
-from face_package.face_location import Location
-from face_package.face_recognition import  Recognition
+from face_package.face_detect import Detect
+from face_package.face_recognition import Recognition
 from face_package.face_emotion import Emotion
 from face_package.face_matting import Matting
 from face import Face
 
-location_model = Location()
-recognition_model = Recognition()
-emotion_model = Emotion()
-matting_model = Matting()
-face_handle = Face(location_model, emotion_model, recognition_model,matting_model)
 
+if "face_handle" not in st.session_state:
+    detect_model = Detect()
+    recognition_model = Recognition()
+    emotion_model = Emotion()
+    matting_model = Matting()
+    face_handle = Face(detect_model, emotion_model,
+                       recognition_model, matting_model)
+    st.session_state["face_handle"] = face_handle
 
-analyzer_button = None
-export_result_button = None
-group_img = None
-group_img_file: UploadedFile = None
-group_img_content = None
-person_img_files: List[UploadedFile] = None
-person_dict: Dict[str, Image] = None
-detect_model_type = None
-recognition_model_type = None
-emotion_model_type = None
-
-st.session_state["group_imag"] = None
-st.session_state["person_images"] = []
+if "person_images" not in st.session_state:
+    st.session_state["person_images"] = {}
+if "group_image" not in st.session_state:
+    st.session_state["group_image"] = None
+if 'analyzer_disabled' not in st.session_state:
+    st.session_state.analyzer_disabled = True
 
 
 # setting page layout
@@ -48,55 +45,55 @@ st.set_page_config(
 )
 
 # main page heading
-st.title("合照人脸识别系统")
+
+container = st.container(border=True)
+
+
+def should_disable_analyzer():
+    return st.session_state.get('analyzer_disabled', True)
+
+
+def submit_click():
+    st.session_state["analyzer_disabled"] = False
+
+
+def analyzer():
+    face_handle: Face = st.session_state["face_handle"]
+    face_handle.add_person_img()
+    origin_col, face_col = container.columns(2)
+
+    with origin_col:
+        origin_col.image(
+            st.session_state["person_images_content"], caption='上传并读取的图像')
+    with face_col:
+        face_col.image(st.session_state["person_images_content"])
+
 
 # sidebar
-st.sidebar.header("模型配置选择")
+with st.sidebar as side:
 
-# model options
+    # model options
+    with st.form("person_image_form", clear_on_submit=True):
+        group_img_file: UploadedFile = st.file_uploader(
+            "上传合照文件", accept_multiple_files=False)
+        person_image_files: List[UploadedFile] = st.file_uploader(
+            "上传个人文件", accept_multiple_files=True)
 
+        sd_col1, sd_col2 = st.columns(2)
 
+        submitted = sd_col1.form_submit_button("提交", on_click=submit_click)
+        analyzer_button = sd_col2.form_submit_button(
+            "开始分析", disabled=should_disable_analyzer())
+        if submitted:
+            logger.info(group_img_file)
+            st.session_state["person_images_content"] = Image.open(
+                group_img_file)
+            st.session_state["person_images"] = []
+            for person_img_file in person_image_files:
+                name_without_extension = os.path.splitext(person_img_file.name)[0]
+                
+                st.session_state["person_images"][name_without_extension] = Image.open(
+                    person_img_file)
 
-if detect_model_type:
-    model_path = Path(config.MODEL_PATH, str(detect_model_type))
-
-
-group_img_file: UploadedFile = st.sidebar.file_uploader(
-    "上传合照文件", accept_multiple_files=False)
-
-
-with st.sidebar.form("person_image_form", clear_on_submit=True):
-    person_image_file = st.file_uploader(
-        "上传个人照文件", accept_multiple_files=True)
-    if person_image_file:
-        person_dict = {}
-        for item in person_image_file:
-            file_path = Path(item.name)
-            file_name_without_ext = file_path.stem
-            person_dict[file_name_without_ext] = Image.open(item)
-        logger.info(f"person dict len {len(person_dict)}")
-        face_handle.add_person_img(person_dict)
-
-    submitted = st.form_submit_button("提交")
-    if submitted:
-        logger.info((len(person_dict)))
-
-
-if group_img_file:
-    logger.info(group_img_file)
-    group_img_content = Image.open(group_img_file)
-    face_handle.add_group_img(group_img_content)
-    st.write("group image is ")
-    # st.image(group_img_content, caption="上传的图片", use_container_width=True)
-    st.image(group_img_content, channels="BGR", caption='上传并读取的图像')
-
-
-    analyzer_button = st.button("开始分析")
-    
-    if analyzer_button:
-        with st.spinner("正在处理，请稍候..."):
-
-            face_handle.detect()
-        st.success("Done!")
-
-
+        if analyzer_button:
+            analyzer()
